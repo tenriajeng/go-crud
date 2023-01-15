8867dfa311d086c24bb5446df1af0f1a224545b8
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2023-01-16 01:11:50 +0800

    update models

--- models/postModel.go
+++ models/postModel.go
@@ -4,6 +4,6 @@ import "gorm.io/gorm"
 
 type Post struct {
 	gorm.Model
-	Title string
-	Body  string
+	Title string `json:"title"`
+	Body  string `json:"body"`
 }
--- models/userModel.go
+++ models/userModel.go
@@ -4,6 +4,6 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Email    string `gorm:"unique"`
-	Password string
+	Email    string `gorm:"unique" json:"email"`
+	Password string `json:"password"`
 }
